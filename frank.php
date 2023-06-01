7887ac617fb3b8c7e2444b5e70048bd52fec3495
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-06-01 09:30:43 -0500

    feat: :sparkles: Class
    Convert to class

--- frank.php
+++ frank.php
@@ -1,50 +1,69 @@
 <?php
-$name1 = "num";
-$value1 = "";
-$name2 = "op";
-$value2 = "";
-
-if (isset($_POST['num'])){
-    $num = $_POST['input'].$_POST['num'];
-}else{
-    $num="";
-}
-
-
-if (isset($_POST['op'])){
-    $value1 = $_POST['input'];
-    setcookie($name1, $value1, time()+(86400*30), "/");
-
-    $value2 = $_POST['op'];
-    setcookie($name2, $value2, time()+(86400*30), "/");
-    $num = "";
-}
-
-
-if (isset($_POST['equal'])){
-    $num = $_POST['input'];
-    switch($_COOKIE['op'])
-    {
-        case "+":
-            $result=$_COOKIE['num']+$num;
-            break;
-            case "-":
-                $result=$_COOKIE['num']-$num;
-                break;
-                case "*":
-                    $result=$_COOKIE['num']*$num;
-                    break;
-                    case "/":
-                        if($_COOKIE['num'] != 0){
-                            $result=$_COOKIE['num']/$num;
-                        }else{
-                            $result = NAN;
-                        }
-                        break;
+class Calculator {
+    private $name1;
+    private $value1;
+    private $name2;
+    private $value2;
+    
+    public function __construct() {
+        $this->name1 = "num";
+        $this->value1 = "";
+        $this->name2 = "op";
+        $this->value2 = "";
     }
-    $num = $result;
+    
+    public function handleInput() {
+        if (isset($_POST['num'])) {
+            $this->num = $_POST['input'] . $_POST['num'];
+        } else {
+            $this->num = "";
+        }
+        
+        if (isset($_POST['op'])) {
+            $this->value1 = $_POST['input'];
+            setcookie($this->name1, $this->value1, time()+(86400*5), "/");
+            
+            $this->value2 = $_POST['op'];
+            setcookie($this->name2, $this->value2, time()+(86400*5), "/");
+            $this->num = "";
+        }
+        
+        if (isset($_POST['equal'])) {
+            $this->num = $_POST['input'];
+            switch ($_COOKIE['op']) {
+                case "+":
+                    $result = $_COOKIE['num'] + $this->num;
+                    break;
+                case "-":
+                    $result = $_COOKIE['num'] - $this->num;
+                    break;
+                case "*":
+                    $result = $_COOKIE['num'] * $this->num;
+                    break;
+                case "/":
+                    if ($_COOKIE['num'] != 0) {
+                        $result = $_COOKIE['num'] / $this->num;
+                    } else {
+                        $result = NAN;
+                    }
+                    break;
+            }
+            $this->num = $result;
+        }
+    }
+
+public function getResult() {
+    return $this->num;
+}
+}
+$calculator = new Calculator();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$calculator->handleInput($_POST);
 }
 ?>
+
+
+
 
 
 
@@ -61,7 +80,7 @@ if (isset($_POST['equal'])){
 <body>
     <div class="calc">
         <form action="" method="post">
-            <input type="text" class="mainInput" name="input" value="<?php echo $num?>"><br>
+            <input type="text" class="mainInput" name="input" value="<?php echo @$calculator->getResult()?>"><br>
             <input type="submit" class="numBtn" name="num" value="7">
             <input type="submit" class="numBtn" name="num" value="8">
             <input type="submit" class="numBtn" name="num" value="9">
@@ -80,7 +99,10 @@ if (isset($_POST['equal'])){
             <input type="submit" class="numBtn" name="c"value="C">
             <input type="submit" class="numBtn" name="num"value="0">
             <input type="submit" class="numBtn" name="op"value="/">
-            <input type="submit" class="numBtn" name="equal"value="=">
+            <input type="submit" class="numBtn" name="equal"value="="><br>
+
+            <input type="submit" class="numBtn" name="num"value=".">
+            <input type="submit" class="numBtn" name="num"value="-">
 
         </form>
     </div>
